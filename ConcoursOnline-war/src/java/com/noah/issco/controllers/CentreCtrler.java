a9c709cfcbd986ef68428290edf850cdbd1dafe1
shelby <shelby@kali-sh>
2016-01-11 08:12:28 +0100

    new commit

--- ConcoursOnline-war/src/java/com/noah/issco/controllers/CentreCtrler.java
+++ ConcoursOnline-war/src/java/com/noah/issco/controllers/CentreCtrler.java
@@ -1,13 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Controller pour les centres d'examen
+ *agrée.
  */
 package com.noah.issco.controllers;
 
+import com.noah.issco.entities.Centre;
+import com.noah.issco.models.CentreFacade;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
 
 /**
  *
@@ -17,10 +20,58 @@ import java.io.Serializable;
 @SessionScoped
 public class CentreCtrler implements Serializable {
 
+    @EJB
+    private CentreFacade centreFacade;
+    private List<Centre> element =null;
+    private Centre selectioner;
+            
+    
+
     /**
      * Creates a new instance of CentreCtrler
      */
     public CentreCtrler() {
     }
+
+    public CentreFacade getCentreFacade() {
+        return centreFacade;
+    }
+
+    public void setCentreFacade(CentreFacade centreFacade) {
+        this.centreFacade = centreFacade;
+    }
+
+    public List<Centre> getElement() {
+        if(element==null){
+            element = getCentreFacade().findAll();
+        }
+        return element;
+    }
+      
+    public void setElement(List<Centre> element) {
+        this.element = element;
+    }
+
+    public Centre getSelectioner() {
+        return selectioner;
+    }
+
+    //obtenir les infos du centre connaissant un id
+    public Centre getCentre(java.lang.Integer id) {
+        return getCentreFacade().find(id);
+    }
+    
+     public List<Centre> getElementAvailableSelectMany() {
+        return getCentreFacade().findAll();
+    }
+
+    public List<Centre> getElementAvailableSelectOne() {
+        return getCentreFacade().findAll();
+    }
+    
+    public void setSelectioner(Centre selectioner) {
+        this.selectioner = selectioner;
+    }
+    
     
 }
